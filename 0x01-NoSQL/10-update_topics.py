1dbe683a4cac1a8b81c78348ffd55307c4e82d6a
shumani <shumanisimeli@gmail.com>
2024-07-16 23:58:24 +0200

    created 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""10-update_topics.py"""
+
+
+def update_topics(mongo_collection, name, topic):
+    """changes all topics of a school document based on the name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    
