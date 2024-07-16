b4c503c0a5173a7b9980e1b777e2706b522a9353
shumani <shumanisimeli@gmail.com>
2024-07-16 22:47:23 +0200

    updated 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """8-all.py"""
+from pymongo.collection import Collection
 from typing import List, Dict
 
+
 def list_all(mongo_collection: Collection) -> List[Dict]:
-    """"""
+    """Lists all documents in a MongoDB collection.
+    Args:
+        mongo_collection: A pymongo collection object.
+    Return:
+        A list of dictionaries.
+    """
     documents = mongo_collection.find()
     return list(documents)
