b3d846935acbb56b2408f45186f3a213f0de7487
shumani <shumanisimeli@gmail.com>
2024-07-17 00:06:14 +0200

    fixed typo on 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,10 +2,9 @@
 """10-update_topics.py"""
 
 
-def update_topics(mongo_collection, name, topic):
+def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
-    
