d9f863017af09edf26f66a9aaa095798fdaf8f72
shumani <shumanisimeli@gmail.com>
2024-07-16 23:23:05 +0200

    created 9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module  inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert anew documents in collection
+    """
+    result = mongo_collection.insert(kwargs)
+    return str(result.inserted_id)
