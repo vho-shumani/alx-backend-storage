2390eb6995a5e5c77e9f8e0342cf835357fb4d10
shumani <shumanisimeli@gmail.com>
2024-07-17 23:26:25 +0200

    updated exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """exercise.py"""
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -29,3 +29,57 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis using given key
+        and appy optional conversation method
+
+        Args:
+            key(str): the key to retrieve
+            fn(optional[callable]): optional callable to
+            convert the retieve data
+
+        Return:
+            The retrieved data, converted, or None if its not it Redis
+        """
+        data = self._redis.get(key)
+        if not data:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+        def get_str(self, key: str) -> str:
+            """
+            Retrieve string
+
+            Args:
+                key(str): key to retieve
+
+            return:
+                return retrieved string data, or None if it doesnt exists.
+            """
+            data = self.get(key)
+            if data:
+                return data.decode("utf-8")
+            else:
+                return None
+
+        def get_int(self, key: str) -> int:
+            """
+            Retrieves interger
+
+            Args:
+                key(str): key to retrieve.
+
+            return:
+                retrieved interger, or None if it doesnt exists.
+            """
+            data = self.get(key)
+            if data:
+                return int(data)
+            else:
+                return None
